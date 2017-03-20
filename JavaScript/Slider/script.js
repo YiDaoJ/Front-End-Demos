3cdc868fa8667617df6a3e24c7fee2ae37d878af
Chuxiao Jiang <jcxsusan@gmail.com>
2017-03-20 22:21:04 +0100

    JS Slider  upload

---
+++ JavaScript/Slider/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+	var slider = document.getElementById('slider');
+	var slideList = document.getElementById('sliderwrapper');
+	var slideCount = slideList.querySelectorAll('li').length;
+	var pointCount = document.getElementById('controller_wrapper').querySelectorAll('li').length;
+	var prev = document.getElementById('prev');
+	var next = document.getElementById('next');
+
+	console.log(slideCount);
+	console.log(pointCount);
+
+
+
+};
